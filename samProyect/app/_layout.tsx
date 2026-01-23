e1808ab0c8358458dce4f4a178c52420e572e2f7
Hector5472 <hectormartinn2005@gmail.com>
2026-01-23 09:47:58 +0000

    Layout cambiado

--- samProyect/app/_layout.tsx
+++ samProyect/app/_layout.tsx
@@ -1,25 +1,7 @@
-import { Stack } from "expo-router";
 import React from "react";
-import { Provider as PaperProvider, DefaultTheme as PaperDefaultTheme } from 'react-native-paper';
-import customTheme from './theme/Theme';
+import { Stack } from "expo-router";
 
-const paperTheme = {
-  ...PaperDefaultTheme,
-  colors: {
-    ...PaperDefaultTheme.colors,
-    primary: customTheme.colors.primary,
-    accent: customTheme.colors.secondary,
-    background: customTheme.colors.background,
-    surface: '#ffffff',
-    text: customTheme.colors.textPrimary,
-  },
-};
-
-function RootLayout() {
-    return (
-      <PaperProvider theme={paperTheme}>
-        <Stack />
-      </PaperProvider>
-    );
+export default function Layout() {
+  return <Stack />;
 }
-export default RootLayout;
+
